d0ffded9654d7a2beec24ef87509ab1c081c8b8b
kaio-mendes <kaio.mendes1911@gmail.com>
2025-06-18 16:23:21 -0300

    formularios

--- src/modules/forms/config-forms.tsx
+++ src/modules/forms/config-forms.tsx
@@ -1,6 +1,9 @@
-import { Box, Drawer, TextField } from '@mui/material';
-import type { SetStateAction } from 'react';
+import type { JSX, SetStateAction } from 'react';
 import type React from 'react';
+import { CreateBeds } from './adm-forms/create-Room-form';
+import { CreateWorker } from './adm-forms/create-worker-form';
+import { CreateSector } from './adm-forms/create-sector-form';
+import { ManageUsers } from './adm-forms/manage-users';
 
 type Form = {
   form: string;
@@ -8,11 +11,12 @@ type Form = {
 };
 
 export const ConfigForms: React.FC<Form> = ({ form, setForm }) => {
-  return (
-    <Drawer anchor="right" open={!!form} onClose={() => setForm('')}>
-      <Box sx={{ padding: 2, alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
-        <h3>Cadastrando {form}:</h3>
-      </Box>
-    </Drawer>
-  );
+  const renderForms: Record<string, JSX.Element> = {
+    Leitos: <CreateBeds form={form} setForm={setForm} />,
+    Profissionais: <CreateWorker form={form} setForm={setForm} />,
+    Setores: <CreateSector form={form} setForm={setForm} />,
+    Usuarios: <ManageUsers form={form} setForm={setForm} />,
+  };
+
+  return renderForms[form] ?? null;
 };
